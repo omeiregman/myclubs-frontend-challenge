12cf00d8f3c362992ce7d80dee0e1a1a31f171c4
= <=>
2017-10-20 12:16:08 +0100

    screen style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import './api/data.js';
 import axios from 'axios';
+//import Link from 'react-router';
 
 class App extends Component {
   constructor(props){
@@ -22,31 +23,42 @@ api=()=>{
     });
 }
 
-componentWillMount(){
+componentDidMount(){
   this.api();
 }
 
   render() {
-  debugger;
   let data = this.state.data;
     return (
       <div className="sitecontainer">
-        {(data!=null) && (<header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="myClubs" />
             <ul className = "navigation__main">
-              <li>Sport partners</li>
-              <li>Sports Search</li>
-              <li>Corporate Fitness</li>
-              <li>Select Tariff</li>
-              <li>{data.reservationEmail}</li>
+              <li><a href="#">Sport partners</a></li>
+              <li><a href="#">Sports Search</a></li>
+              <li><a href="#">Corporate Fitness</a></li>
+              <li><a className="button" href="#">Select Tariff</a></li>
+              <li className="loginpoint"><a className="btn_login" href="#">Login
+                <span className="icon icon--profile"></span></a></li>
+
             </ul>
-          <div className="hero-box">
+          <div className="category-header-poition category__header__content">
+            <div className="category__header__title category__header__title--initialized">
             <h1>Crossfit</h1>
+            </div>
             <p>
               High intensity fitness programme incorporating <br></br>elements from several sports and types of excercise
             </p>
           </div>
-        </header>)}
+        </div>
+
+      {data && (
+        <div className="side-pane">
+          <div>
+            <h4>Address: {data.address}</h4>
+          </div>
+        </div>
+      )}
       </div>
     )
   }
